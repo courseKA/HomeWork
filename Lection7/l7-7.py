6c78d7f8e0a65d44be0d0b5b4e0c1173abe65375
Kalin Anastasov <hydro_proekt@dir.bg>
2024-01-11 03:36:57 +0200

    seven

--- Lection7/l7-7.py
+++ Lection7/l7-7.py
@@ -1,3 +1,23 @@
 '''Задача 7. Напишете програма, която намира максимална редица от последователни
 еднакви елементи в списък.
 Пример: {2, 1, 1, 2, 3, 3, 2, 2, 2, 1} -> {2, 2, 2}. ‘2’ * counter'''
+
+lst = [2,1,1,2,3,3,2,2,2,1]
+# Create empty list
+duplicates = []
+res = []
+# Find equal digits
+for numb in lst:
+    if not duplicates:
+        duplicates.append(numb)
+    else:
+        if numb == duplicates[0]:
+            duplicates.append(numb)
+        elif numb != duplicates[0]:
+            duplicates.clear()
+            duplicates.append(numb)
+        
+    if len(res) < len(duplicates):
+        res = duplicates.copy()
+
+print(res)
