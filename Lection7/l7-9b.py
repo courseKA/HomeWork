ab1c5763030d7dbd5a06bd24c0d85117bb744f0a
Kalin Anastasov <hydro_proekt@dir.bg>
2024-01-20 10:21:53 +0200

    seven

--- Lection7/l7-9b.py
+++ Lection7/l7-9b.py
@@ -6,3 +6,15 @@
 for i,j,m,l in zip(range(1,6), range(8,4,-1),range(9,14), range(16,12, -1)):
 
     print(f'{i}\t{j}\t{m}\t{l}')
+
+#second variant
+for row in range(1,5):
+    column = 1
+    while column <= 4:
+        if column % 2 != 0:
+            col = row + 4*(column - 1)
+        else:
+            col = 4 * column - (row - 1)
+        print(col, end=' ')
+        column += 1
+    print()
